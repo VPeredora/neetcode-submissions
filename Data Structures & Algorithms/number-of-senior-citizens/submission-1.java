467f3e47212e704b366c2cbe9ab92c801b43fde6
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-16 17:07:02 +0200

    Delete Data Structures & Algorithms/number-of-senior-citizens/submission-1.java

--- Data Structures & Algorithms/number-of-senior-citizens/submission-1.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int countSeniors(String[] details) {
-        int number = 0;
-        for (String s : details)
-            if (Integer.parseInt(s.substring(11, 13)) > 60) number++;
-        return number;
-    }
-}
