17d581573ac44bd1740048923878a5d86c2e32e6
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-16 17:26:06 +0200

    Add: max-consecutive-ones - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int pointer = 0, number = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) pointer ++;
+            if (nums[i] == 0 || i == nums.length - 1) {
+                number = number < pointer ? pointer : number;
+                pointer = 0;
+            }
+        }
+        return number;
+    }
+}
