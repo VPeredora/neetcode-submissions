e26150056434765c7aa9771f843283bf8e6de124
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-18 14:18:15 +0200

    Update submission-1.java

--- Data Structures & Algorithms/string-matching-in-an-array/submission-1.java
+++ Data Structures & Algorithms/string-matching-in-an-array/submission-1.java
@@ -1,8 +1,8 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
         Arrays.sort(words, Comparator.comparingInt(String::length));
-
         List<String> substrings = new ArrayList<>();
+        
         for (int i = 0; i < words.length - 1; i++)
             for (int j = i + 1; j < words.length; j++)
                 if (words[j].contains(words[i])) {
