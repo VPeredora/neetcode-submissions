e2ddbbe65d628f207603a0eaef932f8f308b04a5
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-25 15:27:31 +0200

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0, count = 0;
+        for (int i : nums) {
+            if (count == 0) res = i;
+            if (res == i) count++;
+            else count--;
+        }
+        return res;
+    }
+}
