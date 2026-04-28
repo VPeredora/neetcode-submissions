84e8f0e2130f4bb7d39725375f65b03a96b30d4a
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-28 14:30:40 +0200

    Add: longest-strictly-increasing-or-strictly-decreasing-subarray - submission-1

---
+++ Data Structures & Algorithms/longest-strictly-increasing-or-strictly-decreasing-subarray/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestMonotonicSubarray(int[] nums) {
+        int length = 1, tempI = 1, tempD = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            while (i < nums.length - 1 && nums[i] < nums[i + 1]) {
+                tempI++;
+                i++;
+            }
+            while (i < nums.length - 1 && nums[i] > nums[i + 1]) {
+                tempD++;
+                i++;
+            }
+            if (tempI > 1 || tempD > 1) {
+                length = length < tempI ? tempI : length;
+                length = length < tempD ? tempD : length;
+                tempI = 1;
+                tempD = 1;
+                i--;
+            }
+        }
+
+        return length;
+    }
+}
