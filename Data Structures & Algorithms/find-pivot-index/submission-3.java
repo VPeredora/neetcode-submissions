ed3b9331a18189fd4f249ab857ecd3faca86d6bf
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-04-29 23:17:30 +0200

    Add: find-pivot-index - submission-3

---
+++ Data Structures & Algorithms/find-pivot-index/submission-3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sumL = 0, sumR = 0, pivotIndex = 0;
+        for (int i = 1; i < nums.length; i++)
+            sumR += nums[i];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (sumL == sumR) return i;
+            if (i == nums.length - 1) break;
+            sumR -= nums[i + 1];
+            sumL += nums[i];
+        }
+
+        return -1;
+    }
+}
