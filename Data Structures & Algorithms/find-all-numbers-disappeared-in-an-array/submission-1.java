67f3a9037784390fdae4bad78b1fd8bbe9898c78
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-02 01:16:32 +0200

    Update submission-1.java

--- Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-1.java
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-1.java
@@ -3,7 +3,7 @@ class Solution {
         Set<Integer> allNumbers = IntStream.range(1, nums.length + 1).boxed().collect(Collectors.toSet());
         
         for (int i : nums)
-            if (allNumbers.contains(i)) allNumbers.remove(Integer.valueOf(i));
+            allNumbers.remove(i);
         
         return new ArrayList<>(allNumbers);
     }
