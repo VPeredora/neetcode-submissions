d01c0766d3aa48c42b76dd2c50389a7c2d210841
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-01 16:33:21 +0200

    Add: kth-distinct-string-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        Set<String> distinct = new LinkedHashSet<>();
+        Set<String> notDistinct = new HashSet<>();
+
+        for (String str : arr) {
+            if (notDistinct.contains(str)) continue;
+            if (!distinct.add(str)) {
+                distinct.remove(str);
+                notDistinct.add(str);
+            }
+        } 
+
+        return distinct.stream().skip(k - 1).findFirst().orElse("");
+    }
+}
