31bbaf42066f108aea865e699e58f96fef251ce5
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-04 01:52:39 +0200

    Add: maximum-number-of-balloons - submission-3

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character, Integer> frequency = new HashMap<>();
+
+        for (char c : text.toCharArray())
+            if ("balloon".contains(String.valueOf(c)))
+                frequency.merge(c, 1, Integer::sum);
+            
+        if (frequency.size() == 5 && frequency.get('l') >= 2 && frequency.get('o') >= 2) {
+            int countOfSingleChars = Math.min(frequency.get('a'), Math.min(frequency.get('b'), frequency.get('n')));
+            int countOfDoubleChars = Math.min(frequency.get('l') / 2, frequency.get('o') / 2);
+            return Math.min(countOfSingleChars, countOfDoubleChars);
+        }
+
+        return 0;
+    }
+}
