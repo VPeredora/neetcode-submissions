85be62f4343a14a3429653aaf19f1ceb255b0a88
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-04 17:42:56 +0200

    Update submission-0.java

--- Data Structures & Algorithms/height-checker/submission-0.java
+++ Data Structures & Algorithms/height-checker/submission-0.java
@@ -1,9 +1,9 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] expected = Arrays.copyOf(heights, heights.length);
+        int[] expected = Arrays.copyOf(heights, heights.length); 
+        Arrays.sort(expected);
         int result = 0;
 
-        Arrays.sort(expected);
         for(int i = 0; i < heights.length; i++)
             if (heights[i] != expected[i]) result++;
         
