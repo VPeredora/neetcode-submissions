b08f6f9a0573c02d6aafd4bca28b65f25a3d4fcb
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-07 14:33:29 +0200

    Add: number-of-good-pairs - submission-0

---
+++ Data Structures & Algorithms/number-of-good-pairs/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+
+        for (int num : nums)
+            frequency.merge(num, 1, Integer::sum);
+
+        return frequency.values().stream().filter(v -> v > 1)
+               .mapToInt(v -> (v * (v - 1)) / 2).sum();
+    }
+}
