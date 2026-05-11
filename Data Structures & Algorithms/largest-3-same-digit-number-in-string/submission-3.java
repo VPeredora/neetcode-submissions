4b708156fff812973c2dff83305be1531611480f
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-11 18:29:17 +0200

    Add: largest-3-same-digit-number-in-string - submission-3

---
+++ Data Structures & Algorithms/largest-3-same-digit-number-in-string/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String largestGoodInteger(String num) {
+        String[] goodIntegers = new String[]{"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+        
+        for (String number : goodIntegers)
+            if (num.contains(number)) return number;
+
+        return "";
+    }
+}
