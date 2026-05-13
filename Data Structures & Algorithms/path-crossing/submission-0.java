56b93491aafa0e08a8754771a7fe7b5d7c84bcce
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-05-13 14:12:18 +0200

    Add: path-crossing - submission-0

---
+++ Data Structures & Algorithms/path-crossing/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        Map<Integer, Set<Integer>> units = new HashMap<>();
+        int x = 0, y = 0;
+        units.put(x, new HashSet(Set.of(y)));
+
+        for (char quarter : path.toCharArray()) {
+            switch (quarter) {
+                case 'N': {y += 1; break;}
+                case 'S': {y -= 1; break;}
+                case 'E': {x += 1; break;}
+                case 'W': {x -= 1; break;}
+            }
+            if (!units.containsKey(x)) {
+                units.put(x, new HashSet(Set.of(y)));
+                continue;
+            }
+            if (!units.get(x).add(y)) return true;
+        }
+
+        return false;
+    }
+}
