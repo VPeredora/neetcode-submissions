679bf97ad8d30c8811436729b3aea0836af7371f
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-06-01 18:36:12 +0200

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length, middle = 0;
+
+        while (left < right) {
+            middle = left + ((right - left) / 2);
+
+            if (target <= nums[middle]) right = middle;
+            else left = middle + 1;
+        }
+
+        return (left < nums.length && nums[left] == target) ? left : left;
+    }
+}
