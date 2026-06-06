3653181e6ea65ee3192233eb047e5873d20450ed
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-06-06 13:59:45 +0200

    Add: buy-and-sell-crypto - submission-32

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-32.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, buy = prices[0];
+
+        for (int price : prices) {
+            profit = Math.max(profit, price - buy);
+            buy = Math.min(buy, price);
+        }
+
+        return profit;
+    }
+}
