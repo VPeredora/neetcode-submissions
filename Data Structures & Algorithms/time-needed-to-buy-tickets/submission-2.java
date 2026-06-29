402f8143ed642d39255b647e27fa16703d340b3b
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-06-29 18:05:13 +0200

    Add: time-needed-to-buy-tickets - submission-2

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Deque<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < tickets.length; i++)
+            queue.offer(i);
+
+        int time = 0;
+        while (tickets[k] > 0) {
+            int person = queue.poll();
+            tickets[person]--; time++;
+
+            if (tickets[person] > 0) queue.offer(person);;
+        }
+
+        return time;
+    }
+}
