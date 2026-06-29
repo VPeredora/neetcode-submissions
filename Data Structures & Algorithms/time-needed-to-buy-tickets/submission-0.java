105b45493aaaf2397ef6ebe13bc9574adf19d693
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-06-29 17:46:11 +0200

    Add: time-needed-to-buy-tickets - submission-0

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time = 0, person = 0;
+
+        while (tickets[k] > 0) {
+            while (tickets[person % tickets.length] == 0) 
+                person++;
+            tickets[person % tickets.length]--;
+            person++;
+            time++;
+        }
+
+        return time;
+    }
+}
