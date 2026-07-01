fbe19077f847f68e404ce6b88f3c580619cc9b43
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-07-01 23:19:33 +0200

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        shellSort(nums);
+        return nums;
+    }
+
+    private static void shellSort(int[] arr) {
+        int n = arr.length;
+
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                int key = arr[i];
+                int j = i;
+                while (j >= gap && arr[j - gap] > key) {
+                    arr[j] = arr[j - gap];
+                    j -= gap;
+                }
+                arr[j] = key;
+            }
+        }
+    }
+}
