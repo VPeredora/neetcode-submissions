302a66a43d2aa218c7dccb7a538b6be37eda1090
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-07-04 17:23:41 +0200

    Add: custom-sort-string - submission-1

---
+++ Data Structures & Algorithms/custom-sort-string/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        int[] rank = new int[26];
+        for (int i = 0; i < order.length(); i++)
+            rank[order.charAt(i) - 'a'] = i + 1;
+        
+        Character[] arr = new Character[s.length()];
+        for (int i = 0; i < s.length(); i++)
+            arr[i] = s.charAt(i);
+        
+        Arrays.sort(arr, (a, b) -> Integer.compare(rank[a - 'a'], rank[b - 'a']));
+
+        StringBuilder result = new StringBuilder(s.length());
+        for (char c : arr)
+            result.append(c);
+        
+        return result.toString();
+    }
+}
