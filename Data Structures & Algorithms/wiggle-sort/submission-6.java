35098077e6053ed47736a1f85e1a2c35bb62d9f2
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-07-20 21:38:59 +0200

    Add: wiggle-sort - submission-6

---
+++ Data Structures & Algorithms/wiggle-sort/submission-6.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++)
+            if (((i % 2) ^ (nums[i] > nums[i - 1] ? 1 : 0)) != 0) {
+                int temp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i - 1] = temp;
+            }
+    }
+}
