744132ff17d41a58820f56683bc7e1863efcf9a1
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-09-02 13:11:09 +0200

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] count = new int[26];
+        for (char c : s1.toCharArray())
+            count[c - 'a']++;
+        
+        for (int l = 0; l < s2.length(); l++) {
+            int[] count2 = new int[26];
+
+            for (int r = l; r < s2.length(); r++) {
+                char c = s2.charAt(r);
+                count2[c - 'a']++;
+
+                if (count2[c - 'a'] > count[c - 'a']) break;
+                if (r - l + 1 == s1.length()) return true;
+            }
+        }
+        
+        return false;
+    }
+}
