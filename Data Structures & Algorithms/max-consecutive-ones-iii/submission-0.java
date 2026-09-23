068806c67559681e773a06355478d375775fc32e
Vladyslav Peredora <101666355+VPeredora@users.noreply.github.com>
2026-09-23 19:27:08 +0200

    Add: max-consecutive-ones-iii - submission-0

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l = 0, r = 0, zeros = 0;
+
+        for (; r < nums.length; r++) {
+            if (nums[r] == 0) zeros++;
+
+            if (zeros > k)
+                if (nums[l++] == 0) zeros--;
+        }
+
+        return r - l;
+    }
+}
